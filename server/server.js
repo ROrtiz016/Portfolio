865f223e23b5c137b65cf412ebbb03e4f451dacc
ROrtiz016 <37273928+ROrtiz016@users.noreply.github.com>
2018-09-13 14:37:19 -0600

    working app

--- server/server.js
+++ server/server.js
@@ -6,10 +6,6 @@ const express = require('express')
 const app = express()
 const port = 3006
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-});
-
 app.use(express.static(`${__dirname}/../build`))
 
 // Generate test SMTP service account from ethereal.email
