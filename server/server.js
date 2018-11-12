481f21a4a06fae189ecf5b3842559eb165ead260
ROrtiz016 <37273928+ROrtiz016@users.noreply.github.com>
2018-11-12 13:46:50 -0700

    nodemailertest

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const port = 3006
+const port = 465
 
 app.use(bodyParser.json())
 
